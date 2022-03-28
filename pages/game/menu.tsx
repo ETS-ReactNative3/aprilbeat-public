2fcc23402ef6b11cd91875977334a0b25d4cacf1
SpaceXLiquid <64797361+SpaceXLiquid@users.noreply.github.com>
2022-03-28 20:43:39 +0700

    Update menu.tsx

--- pages/game/menu.tsx
+++ pages/game/menu.tsx
@@ -64,25 +64,25 @@ export default function Menu({ dataProps } : {dataProps: AppDataProps}) {
                         <div id="friends" className={`grid grid-flow-row-dense grid-rows-1 grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-x-2 gap-y-3 items-top w-full space-x-3 mt-5`}>
                             <button id="friend1" className={`h-full hover:bg-gray-100 transition-all dark:hover:bg-[#202020] text-center px-4 py-3 rounded-xl flex flex-col items-center`}>
                                 <img className={`w-16 h-16 rounded-full`} src={`/assets/mikosunePfp.webp`} />
-                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Farrell</h1>
+                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>lav</h1>
                                 <h3 className={`text-gray-400 text-xs`}>Seen 3h ago</h3>
                             </button>
 
                             <button id="friend2" className={`h-full hover:bg-gray-100 transition-all dark:hover:bg-[#202020] text-center px-4 py-3 rounded-xl flex flex-col items-center justify-top`}>
                                 <img className={`w-16 h-16 rounded-full `} src={`/assets/mikosunePfp.webp`} />
-                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Agnetha</h1>
+                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Lilith</h1>
                                 <h3 className={`text-green-400 text-xs`}>Online</h3>
                             </button>
 
                             <button id="friend3" className={`h-full hover:bg-gray-100 transition-all dark:hover:bg-[#202020] text-center px-4 py-3 rounded-xl flex flex-col items-center`}>
                                 <img className={`w-16 h-16 rounded-full `} src={`/assets/mikosunePfp.webp`} />
-                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Evelyn</h1>
+                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Abel</h1>
                                 <h3 className={`text-yellow-400 text-xs`}>Busy</h3>
                             </button>
 
                             <button id="friend4" className={`h-full hover:bg-gray-100 transition-all dark:hover:bg-[#202020] text-center px-4 py-3 rounded-xl flex flex-col items-center`}>
                                 <img className={`w-16 h-16 rounded-full `} src={`/assets/mikosunePfp.webp`} />
-                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Beverly</h1>
+                                <h1 className={`mt-3 text-sm font-semibold text-black dark:text-white`}>Jude</h1>
                                 <h3 className={`text-red-400 text-xs`}>Do not disturb</h3>
                             </button>
 
