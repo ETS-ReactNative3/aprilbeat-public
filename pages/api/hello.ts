8bc43a7a5fc180b5549d18aa18c8cd2c2b84522c
Afnan <72243953+afnan007a@users.noreply.github.com>
2022-03-29 17:17:20 +0530

    fixed timestamp

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ Time_Stamp : 'The current timestamp is ' + new Date().getTime()})
+  res.status(200).json({ timestamp: 'The current timestamp is ' + new Date().getTime()})
 }
