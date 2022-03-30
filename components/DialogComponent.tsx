0989d1878b7239d63feab2c7e2122cc9165141c2
SpaceXLiquid <64797361+SpaceXLiquid@users.noreply.github.com>
2022-03-30 10:26:54 +0700

    Added error boundaries

--- components/DialogComponent.tsx
+++ components/DialogComponent.tsx
@@ -1,5 +1,7 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
+import useErrorBoundary from "use-error-boundary"
+import ErrorCatcher from './ErrorCatcher';
 
 interface DialogComponent {
   title?: string;
@@ -15,6 +17,13 @@ interface DialogComponent {
 }
 
 export default function DialogComp({ title, description, state, stateSetter, buttonDisabled = false, dismissable = true, buttonText = 'Got It, thanks!', callback = () => null, raw = false, children }:DialogComponent) {
+
+  const {
+    ErrorBoundary,
+    didCatch,
+    error,
+    reset
+  } = useErrorBoundary()
 
     const dialogIsOpen = state
     const setDialogIsOpen = stateSetter
@@ -33,76 +42,84 @@ export default function DialogComp({ title, description, state, stateSetter, but
     }
 
     return (
-
-  <Transition appear show={dialogIsOpen} as={Fragment}>
-        <div className="w-screen h-screen hidden">
-        <Dialog
-          as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          onClose={() => dismissDialog()}
-        >
-          <div className="min-h-screen px-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+      <div>
+        <ErrorCatcher
+          didCatch={didCatch}
+          error={error}
+          area={`component_dialog`}
+        />
+        <ErrorBoundary>
+          <Transition appear show={dialogIsOpen} as={Fragment}>
+            <div className="w-screen h-screen hidden">
+            <Dialog
+              as="div"
+              className="fixed inset-0 z-10 overflow-y-auto"
+              onClose={() => dismissDialog()}
             >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-800 opacity-20 transition-opacity" />
-            </Transition.Child>
-
-            {/* This element is to trick the browser into centering the modal contents. */}
-            <span
-              className="inline-block h-screen align-middle"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-
-                <div id="contentDialog" className={`${raw ? 'hidden' : ''}`}>
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+              <div className="min-h-screen px-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
                 >
-                  {title}
-                </Dialog.Title>
-                <div className="mt-1">
-                  <p className="text-sm text-gray-500">
-                    {description}
-                  </p>
-                </div>
+                  <Dialog.Overlay className="fixed inset-0 bg-gray-800 opacity-20 transition-opacity" />
+                </Transition.Child>
 
-                {children}
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                  className="inline-block h-screen align-middle"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
 
-                <div className={`mt-3 ${dismissable ? '' : 'hidden'}`}>
-                  <button
-                    type="button"
-                    disabled={buttonDisabled ? true : false}
-                    className="inline-flex shadow-md justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={() => dismissDialog()}
-                  >
-                    {buttonText}
-                  </button>
-                </div>
-                </div>
+                    <div id="contentDialog" className={`${raw ? 'hidden' : ''}`}>
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      {title}
+                    </Dialog.Title>
+                    <div className="mt-1">
+                      <p className="text-sm text-gray-500">
+                        {description}
+                      </p>
+                    </div>
+
+                    {children}
+
+                    <div className={`mt-3 ${dismissable ? '' : 'hidden'}`}>
+                      <button
+                        type="button"
+                        disabled={buttonDisabled ? true : false}
+                        className="inline-flex shadow-md justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                        onClick={() => dismissDialog()}
+                      >
+                        {buttonText}
+                      </button>
+                    </div>
+                    </div>
+                  </div>
+                </Transition.Child>
               </div>
-            </Transition.Child>
-          </div>
-        </Dialog>
-        </div>
-      </Transition>
+            </Dialog>
+            </div>
+          </Transition>
+        </ErrorBoundary>
+      </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,17 @@ import { AppDataProps } from '@/constants/customTypings/app'
 import type { AuthSession, AuthUser } from '@supabase/supabase-js'
 import type { Users } from '@prisma/client'
 import * as api from '@/clients/apiPublic'
+import useErrorBoundary from "use-error-boundary"
+import ErrorCatcher from '@/components/ErrorCatcher'
 
 function MyApp({ Component, pageProps }) {
+
+  const {
+    ErrorBoundary,
+    didCatch,
+    error,
+    reset
+  } = useErrorBoundary()
 
   const pageTransitionAnimationControl = useAnimation()
   const [inTransition, setInTransition] = useState(false)
@@ -79,34 +88,40 @@ function MyApp({ Component, pageProps }) {
         console.log(userdt)
         setUserData(userdt)
       })
-
   }, [])
   
 
   return (
     <div>
-      <Head>
-        <title>Aprilbeat</title>
-        <meta name="title" content="Aprilbeat" />
-        <meta name="description" content="rythm game shouldn't be limited. Now publicly available, running fully on web." />
-      </Head>
-      <div>
-        <script src="/libs/lowLag.js" />
-        <script src="/libs/soundmanager2-jsmin.js" />
-        <noscript>Please enable Javascript to continue.</noscript>
-      </div>
-
-      <StrictMode>
+      <ErrorCatcher
+        didCatch={didCatch}
+        error={error}
+        area={`app_document`}
+      />
+      <ErrorBoundary>
+        <Head>
+          <title>Aprilbeat</title>
+          <meta name="title" content="Aprilbeat" />
+          <meta name="description" content="rythm game shouldn't be limited. Now publicly available, running fully on web." />
+        </Head>
         <div>
-          <motion.div
-              initial={{ translateX: '-100vw' }}
-              transition={{ ease: 'easeOut', duration: 0.2 }}
-              animate={pageTransitionAnimationControl}
-              className={`absolute w-full z-10 h-screen bg-gray-800`}
-          />
-          <Component {...pageProps} dataProps={dataProps} />
+          <script src="/libs/lowLag.js" />
+          <script src="/libs/soundmanager2-jsmin.js" />
+          <noscript>Please enable Javascript to continue.</noscript>
         </div>
-      </StrictMode>
+
+        <StrictMode>
+          <div>
+            <motion.div
+                initial={{ translateX: '-100vw' }}
+                transition={{ ease: 'easeOut', duration: 0.2 }}
+                animate={pageTransitionAnimationControl}
+                className={`absolute w-full z-10 h-screen bg-gray-800`}
+            />
+            <Component {...pageProps} dataProps={dataProps} />
+          </div>
+        </StrictMode>
+      </ErrorBoundary>
     </div>
   )
 }
