6973582cd74658eef15edd7a7e103ac73d775269
SpaceXLiquid <64797361+SpaceXLiquid@users.noreply.github.com>
2022-03-31 11:50:22 +0700

    A lot of improvements for caching and audio engine
    - Fixed a security issue where checkUserToken was in the publicApi file
    - Added tryCatch in API Json Parsing to further show the error better
    - Added load waiting for the audio api
    - setGlobalVolume is now a thing and is available to set the global volume to all audios playing
    - geGlobalVolume is also now a thing and allows to fetch all audio's volume and get average from all of them.
    - Added new direct cdn bucket to fetch audio (*beta* - seems to be very slow and finicky)
    - Improved implementation of communicating with the audio api
    - Added Audio Queueing from user's playlist (*beta*)
    - Better synchronization between clicks of the aprilbeat icon and song using bpm data

---
+++ pages/api/storage/bucket/songs/[songid]/object.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "@/clients/supabase";
+import { prismaClient } from "@/clients/prisma";
+import { rejectHandler, resolveHandler } from "@/clients/api";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const { songid } = req.query
+
+    const thesong = await prismaClient.songs.findFirst({
+      where: {
+        id: songid.toString()
+      }
+    })
+    if (!thesong) return rejectHandler(res, {
+      code: 'song/not_found',
+      reason: 'Song not found in database',
+      status: 404
+    })
+    const songbucketname = thesong.filename as string;
+
+  supabase.storage
+    .from("songs")
+    .download(songbucketname)
+    .then((data) => {
+      data.data?.stream().pipe(res)
+      // resolveHandler(res, {
+      //   data: {
+      //     songData: thesong,
+      //     signedURL: data.signedURL,
+      //   }
+      // })
+    });
+
+    // supabase.storage
+    //   .from("songs")
+    //   .download("geoxor_svrge_1")
+    //   .then((data) => {
+    //     console.log(data);
+    //     data.data?.stream().pipe(res);
+    //   });
+}
